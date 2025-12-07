2d957c1ceeb7b48e546dc8d025dbe0a8abb01c4b
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-07 04:58:53 -0800

    Update ymlToJson.sh

--- scripts/ymlToJson.sh
+++ scripts/ymlToJson.sh
@@ -4,7 +4,7 @@ alias yq="yq --yaml-fix-merge-anchor-to-spec=true"
 i="$1"
 o="dist/${1#src/}"
 case "${1#*.}" in
-	yml)	o="${o%.yml}.json";;
+	yml)		o="${o%.yml}.json";;
 	mcmeta.yml)	o="${o%.yml}";;
 	*);;
 esac
