8d83dfeb0dbcf2f810315499ae11a9e8105a3154
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-07 04:58:29 -0800

    Update ymlToJson.sh

--- scripts/ymlToJson.sh
+++ scripts/ymlToJson.sh
@@ -2,8 +2,8 @@
 shopt -s expand_aliases
 alias yq="yq --yaml-fix-merge-anchor-to-spec=true"
 i="$1"
-o="dist/${i#src/}"
-case "${i#*.}" in
+o="dist/${1#src/}"
+case "${1#*.}" in
 	yml)o="${o%.yml}.json";;
 	mcmeta.yml)o="${o%.yml}";;
 	*);;
