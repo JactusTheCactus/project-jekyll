0c8a70e80a5ea3c616a524289791f16faa2edf14
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-12-07 20:53:08 -0800

    -

--- scripts/ymlToJson.sh
+++ scripts/ymlToJson.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 shopt -s expand_aliases
 alias yq="yq --yaml-fix-merge-anchor-to-spec=true"
-i="$1"
-o="dist/${1#src/}"
-o="${o%.yml}"
-case "${1#*.}" in
-	yml)o="$o.json";;
-esac
-yq -p yaml -o json "$i" > "$o"
+yq -p yaml -o json "$1" > "$(
+	o="dist/${1#src/}"
+	o="${o%.yml}"
+	case "${1#*.}" in
+		yml)echo "$o.json";;
+		*)echo "$o";;
+	esac
+)"
